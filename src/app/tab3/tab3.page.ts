cb7ec02006fa3f14ee0f1f238ee60cf6ac165f5b
Davide Maggi <davide.maggi@live.com>
2021-12-02 22:55:50 +0100

    aaa

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,50 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { NfcService } from '../services/nfc.service';
-import { ToastController } from '@ionic/angular';
-import { nfcResult } from '../models/nfcResult.model';
-import { SoundService } from '../services/sound.service';
-
+import { Component, OnInit } from "@angular/core";
+import { NfcService } from "../services/nfc.service";
+import { ToastController } from "@ionic/angular";
+import { nfcResult } from "../models/nfcResult.model";
+import { SoundService } from "../services/sound.service";
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  selector: "app-tab3",
+  templateUrl: "tab3.page.html",
+  styleUrls: ["tab3.page.scss"],
 })
-export class Tab3Page implements OnInit{
+export class Tab3Page implements OnInit {
+  public readJson: object = {};
 
-  constructor(private nfcSrvice:NfcService,private toastController: ToastController, private soundService:SoundService) {
+  constructor(
+    private nfcSrvice: NfcService,
+    private toastController: ToastController,
+    private soundService: SoundService
+  ) {}
 
-
+  ngOnInit() {
+    this.nfcSrvice.nfcRead.subscribe((res) => {
+      this.receivedNfc(res);
+    });
   }
 
 
-  ngOnInit() {
-    this.nfcSrvice.nfcRead.subscribe(res=>{
-this.toastNfcRead(res);
-      if(res.isOk){
-this.soundService.playAudio("mucca_1.mp3")
-      }else{
-        
+  receivedNfc(res:nfcResult){
+    console.log(res.getContent());
+      this.readJson = res.getContent();
+
+      this.toastNfcRead(res);
+      if (res.isOk) {
+        //this.soundService.playAudio("mucca_1.mp3")
+      } else {
       }
+  }
 
 
-    }
-        
-      
-
-    );
-
-}
-
-
-async toastNfcRead(res:nfcResult){
-  const toast = await this.toastController.create({
-    message: 'Read Nfc: '+res.content,
-    color:res.isOk() ? "success":"danger",
-    duration: 2000
-  });
-  toast.present();
-}
-
-
+  async toastNfcRead(res: nfcResult) {
+    const toast = await this.toastController.create({
+      message: "Read Nfc: " + res.content,
+      color: res.isOk() ? "success" : "danger",
+      duration: 2000,
+    });
+    toast.present();
+  }
 }
